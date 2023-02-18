a77017604e883016d3b5ab7d35b077e47e36773a
Luis Fernando Machado <>
2023-02-17 23:46:49 -0300

    appending text dynamic into a textview

--- app/src/main/java/com/materialdesign/changethetextcolorofasubstring/MainActivity.java
+++ app/src/main/java/com/materialdesign/changethetextcolorofasubstring/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 import android.text.style.BackgroundColorSpan;
 import android.text.style.ForegroundColorSpan;
 import android.widget.TextView;
@@ -23,11 +24,14 @@ public class MainActivity extends AppCompatActivity {
 		ForegroundColorSpan fcsGreen = new ForegroundColorSpan(Color.GREEN);
 		BackgroundColorSpan bcsYellow = new BackgroundColorSpan(Color.YELLOW);
 		SpannableString ss = new SpannableString(text);
+		SpannableStringBuilder ssb = new SpannableStringBuilder(text);
 
-		ss.setSpan(fcsRed, 7, 11, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-		ss.setSpan(fcsGreen, 16, 20, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-		ss.setSpan(bcsYellow, 27, 34, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		ssb.setSpan(fcsRed, 7, 11, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		ssb.setSpan(fcsGreen, 16, 20, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		ssb.setSpan(bcsYellow, 27, 34, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-		textView.setText(ss);
+		ssb.append("\nand this to be appended");
+
+		textView.setText(ssb);
 	}
 }
